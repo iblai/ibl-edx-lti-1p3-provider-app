d824fc7a010e6643172a4fb50e1741dc7ef7f3a5
Geoff Von Allmen <geoff@ibleducation.com>
2023-07-27 14:28:47 -0700

    refactor: re_path from urls.py - done in app conf

--- src/lti_1p3_provider/urls.py
+++ src/lti_1p3_provider/urls.py
@@ -1,19 +1,12 @@
 """
 URL configuration for LTI 1.3 Provider
 """
-from django.urls import include, path, re_path
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    re_path(
-        r"^lti/1.3/",
-        include(
-            [
-                path("login/", views.LtiToolLoginView.as_view(), name="lti-login"),
-                path("launch/", views.LtiToolLaunchView.as_view(), name="lti-launch"),
-                path("pub/jwks/", views.LtiToolJwksView.as_view(), name="lti-pub-jwks"),
-            ]
-        ),
-    ),
+    path("login/", views.LtiToolLoginView.as_view(), name="lti-login"),
+    path("launch/", views.LtiToolLaunchView.as_view(), name="lti-launch"),
+    path("pub/jwks/", views.LtiToolJwksView.as_view(), name="lti-pub-jwks"),
 ]
