54806f087a6eeb88dcf3af616b272dde6121e94e
Geoff V <geoff@ibleducation.com>
2023-08-10 19:21:56 +0000

    test: fix test_auth imports; black fmt

--- src/lti_1p3_provider/tests/test_auth.py
+++ src/lti_1p3_provider/tests/test_auth.py
@@ -6,9 +6,9 @@ Unit tests for Content Libraries authentication module.
 from django.test import TestCase
 
 
-from ..models import LtiProfile
-from ..models import get_user_model
-from ..auth import LtiAuthenticationBackend
+from lti_1p3_provider.models import LtiProfile
+from lti_1p3_provider.models import get_user_model
+from lti_1p3_provider.auth import Lti1p3AuthenticationBackend
 
 
 class LtiAuthenticationBackendTest(TestCase):
@@ -16,20 +16,21 @@ class LtiAuthenticationBackendTest(TestCase):
     AuthenticationBackend tests.
     """
 
-    iss = 'http://foo.bar'
-    aud = 'a-random-test-aud'
-    sub = 'a-random-test-sub'
+    iss = "http://foo.bar"
+    aud = "a-random-test-aud"
+    sub = "a-random-test-sub"
 
     def test_without_profile(self):
-        get_user_model().objects.create(username='foobar')
-        backend = LtiAuthenticationBackend()
+        get_user_model().objects.create(username="foobar")
+        backend = Lti1p3AuthenticationBackend()
         user = backend.authenticate(None, iss=self.iss, aud=self.aud, sub=self.sub)
         self.assertIsNone(user)
 
     def test_with_profile(self):
         profile = LtiProfile.objects.create(
-            platform_id=self.iss, client_id=self.aud, subject_id=self.sub)
-        backend = LtiAuthenticationBackend()
+            platform_id=self.iss, client_id=self.aud, subject_id=self.sub
+        )
+        backend = Lti1p3AuthenticationBackend()
         user = backend.authenticate(None, iss=self.iss, aud=self.aud, sub=self.sub)
         self.assertIsNotNone(user)
         self.assertEqual(user.contentlibraries_lti_profile, profile)
