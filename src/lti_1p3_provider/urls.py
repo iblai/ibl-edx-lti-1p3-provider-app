d11e0ed5de0ae25557c710af1c422699fcd544fb
Geoff V <geoff@ibleducation.com>
2023-08-14 23:41:34 +0000

    refactor: Use CourseId/UsageKey in url launch path
    - This matches the existing LTI 1.1 implementation
    - It's unclear if it's really necessary since _typically_ course_key
      is embedded in usage_key, but maybe not if it's a library

--- src/lti_1p3_provider/urls.py
+++ src/lti_1p3_provider/urls.py
@@ -1,12 +1,17 @@
 """
 URL configuration for LTI 1.3 Provider
 """
-from django.urls import path
+from django.urls import path, re_path
+from django.conf import settings
 
 from . import views
 
 urlpatterns = [
     path("login/", views.LtiToolLoginView.as_view(), name="lti-login"),
-    path("launch/", views.LtiToolLaunchView.as_view(), name="lti-launch"),
+    re_path(
+        f"launch/{settings.COURSE_ID_PATTERN}/{settings.USAGE_ID_PATTERN}$",
+        views.LtiToolLaunchView.as_view(),
+        name="lti-launch",
+    ),
     path("pub/jwks/", views.LtiToolJwksView.as_view(), name="lti-pub-jwks"),
 ]
