a2865836b65c2f5cdb064f6183697dd9a4356f4c
Geoff V <geoff@ibleducation.com>
2023-08-15 18:57:04 +0000

    refactor: Bring in lti 1.1 score_changed_handler
    - Update it to work for our needs

--- src/lti_1p3_provider/signal_handlers.py
+++ src/lti_1p3_provider/signal_handlers.py
@@ -6,17 +6,15 @@ import logging
 
 from django.conf import settings
 from django.dispatch import receiver
-
 from lms.djangoapps.grades.api import signals as grades_signals
-from opaque_keys import (
-    InvalidKeyError,
-)  # lint-amnesty, pylint: disable=wrong-import-order
-from opaque_keys.edx.locator import (
-    LibraryUsageLocatorV2,
-)  # lint-amnesty, pylint: disable=wrong-import-order
+from opaque_keys import InvalidKeyError
+from opaque_keys.edx.keys import LearningContextKey
+from opaque_keys.edx.locator import LibraryUsageLocatorV2
 
+from .grades import increment_assignment_versions
 from .models import LtiGradedResource
-
+from .tasks import send_composite_score, send_leaf_score
+from .views import parse_course_and_usage_keys
 
 log = logging.getLogger(__name__)
 
@@ -24,49 +22,58 @@ log = logging.getLogger(__name__)
 @receiver(grades_signals.PROBLEM_WEIGHTED_SCORE_CHANGED)
 def score_changed_handler(sender, **kwargs):  # pylint: disable=unused-argument
     """
-    Match the score event to an LTI resource and update.
+    Consume signals that indicate score changes. See the definition of
+    PROBLEM_WEIGHTED_SCORE_CHANGED for a description of the signal.
+
+    NOTE: This was taken from lms.djangoapps.lti_provider.signals and updated for our
+    needs.
     """
-    # NOTE: I think we need to do this more like edx originally did it in lti 1.1
-    # NOTE: This would only work for grading a specific resource that was launched
+    modified = kwargs.get("modified", None)
+    points_possible = kwargs.get("weighted_possible", None)
+    points_earned = kwargs.get("weighted_earned", None)
+    user_id = kwargs.get("user_id", None)
+    course_id = kwargs.get("course_id", None)
+    usage_id = kwargs.get("usage_id", None)
 
-    # TODO: Change to our checks
-    lti_enabled = settings.FEATURES.get(
-        "ENABLE_CONTENT_LIBRARIES"
-    ) and settings.FEATURES.get("ENABLE_CONTENT_LIBRARIES_LTI_TOOL")
-    if not lti_enabled:
+    # Make sure this came from a course because this code only works with courses
+    if not course_id:
         return
+    context_key = LearningContextKey.from_string(course_id)
+    if not context_key.is_course:
+        return  # This is a content library or something else...
 
-    modified = kwargs.get("modified")
-    usage_id = kwargs.get("usage_id")
-    user_id = kwargs.get("user_id")
-    weighted_earned = kwargs.get("weighted_earned")
-    weighted_possible = kwargs.get("weighted_possible")
-
-    if None in (modified, usage_id, user_id, weighted_earned, weighted_possible):
-        log.debug(
-            "LTI 1.3: Score Signal: Missing a required parameters, "
-            "ignoring: kwargs=%s",
-            kwargs,
-        )
-        return
-    try:
-        # TODO: Get our UsageKey
-        usage_key = LibraryUsageLocatorV2.from_string(usage_id)
-    except InvalidKeyError:
-        log.debug(
-            "LTI 1.3: Score Signal: Not a content libraries v2 usage key, "
-            "ignoring: usage_id=%s",
-            usage_id,
-        )
-        return
-    try:
-        resource = LtiGradedResource.objects.get_from_user_id(
-            user_id, usage_key=usage_key
-        )
-    except LtiGradedResource.DoesNotExist:
-        log.debug(
-            "LTI 1.3: Score Signal: Unknown resource, ignoring: kwargs=%s", kwargs
-        )
+    if None not in (points_earned, points_possible, user_id, course_id, modified):
+        course_key, usage_key = parse_course_and_usage_keys(course_id, usage_id)
+        resources = increment_assignment_versions(course_key, usage_key, user_id)
+        for resource in resources:
+            if resource.usage_key == usage_key:
+                send_leaf_score.delay(
+                    resource.id, points_earned, points_possible, modified
+                )
+                log.debug("Created LTI 1.3 leaf score update task")
+            else:
+                send_composite_score.apply_async(
+                    (
+                        user_id,
+                        course_id,
+                        resource.id,
+                        resource.version_number,
+                        modified,
+                    ),
+                    countdown=settings.LTI_AGGREGATE_SCORE_PASSBACK_DELAY,
+                )
+                log.debug("Created LTI 1.3 composite score update task")
     else:
-        resource.update_score(weighted_earned, weighted_possible, modified)
-        log.info("LTI 1.3: Score Signal: Grade upgraded: resource; %s", resource)
+        log.error(
+            (
+                "LTI 1.3 Grades Service: Required signal parameter is None. "
+                "points_possible: %s, points_earned: %s, user_id: %s, "
+                "course_id: %s, usage_id: %s, modified: %s"
+            ),
+            points_possible,
+            points_earned,
+            user_id,
+            course_id,
+            usage_id,
+            modified,
+        )
