deae1e9f9ee31e6d7e11594bad6938b4b67e569a
Geoff V <geoff@ibleducation.com>
2023-08-15 18:50:26 +0000

    refactor: Make send_*_score work for our use case
    - Use LtiGradedResource instead of GradedAssignment
    - Pass modified time in
    - send_leaf/compose_outcome -> send_leaf/composite_score

--- src/lti_1p3_provider/tasks.py
+++ src/lti_1p3_provider/tasks.py
@@ -5,22 +5,22 @@ Asynchronous tasks for the LTI provider app.
 
 import logging
 
-from django.contrib.auth.models import User  # lint-amnesty, pylint: disable=imported-auth-user
+from django.contrib.auth.models import User
 from edx_django_utils.monitoring import set_code_owner_attribute
-from opaque_keys.edx.keys import CourseKey
-
-import lms.djangoapps.lti_provider.outcomes as outcomes
 from lms import CELERY_APP
 from lms.djangoapps.grades.api import CourseGradeFactory
-from lms.djangoapps.lti_provider.models import GradedAssignment
-from xmodule.modulestore.django import modulestore  # lint-amnesty, pylint: disable=wrong-import-order
+from opaque_keys.edx.keys import CourseKey
+from xmodule.modulestore.django import modulestore
+
+from .models import LtiGradedResource
 
 log = logging.getLogger(__name__)
 
 
-@CELERY_APP.task(name='lms.djangoapps.lti_provider.tasks.send_composite_outcome')
+# NOTE: Why are we naming this task?
+@CELERY_APP.task(name="lti_1p3_provider.tasks.send_composite_score")
 @set_code_owner_attribute
-def send_composite_outcome(user_id, course_id, assignment_id, version):
+def send_composite_score(user_id, course_id, resource_id, version, modified):
     """
     Calculate and transmit the score for a composite module (such as a
     vertical).
@@ -37,50 +37,51 @@ def send_composite_outcome(user_id, course_id, assignment_id, version):
     its transaction is committed), it will see a stale value. Care should be
     taken that this task is not triggered until the view exits.
 
-    The GradedAssignment model has a version_number field that is incremented
+    The LtiGradedResource model has a version_number field that is incremented
     whenever the score is updated. It is used by this method for two purposes.
     First, it allows the task to exit if it detects that it has been superseded
     by another task that will transmit the score for the same assignment.
     Second, it prevents a race condition where two tasks calculate different
     scores for a single assignment, and may potentially update the campus LMS
     in the wrong order.
+
+    NOTE: ibl: technically order shouldn't matter for lti 1.3 since we send a modified
+    timestamp and the LMS should ignore timestamps older than the last submitted one.
+
+    NOTE: ibl: Taken from lms.djangoapps.lti_provider.tasks and adjusted for our needs.
     """
-    assignment = GradedAssignment.objects.get(id=assignment_id)
-    if version != assignment.version_number:
+    resource = LtiGradedResource.objects.get(id=resource_id)
+    if version != resource.version_number:
         log.info(
-            "Score passback for GradedAssignment %s skipped. More recent score available.",
-            assignment.id
+            (
+                "Score passback for LtiGradedResource %s skipped. More recent score "
+                "available."
+            ),
+            resource.id,
         )
         return
+
     course_key = CourseKey.from_string(course_id)
-    mapped_usage_key = assignment.usage_key.map_into_course(course_key)
+    mapped_usage_key = resource.usage_key.map_into_course(course_key)
     user = User.objects.get(id=user_id)
     course = modulestore().get_course(course_key, depth=0)
     course_grade = CourseGradeFactory().read(user, course)
     earned, possible = course_grade.score_for_block(mapped_usage_key)
-    if possible == 0:
-        weighted_score = 0
-    else:
-        weighted_score = float(earned) / float(possible)
 
-    assignment = GradedAssignment.objects.get(id=assignment_id)
-    if assignment.version_number == version:
-        outcomes.send_score_update(assignment, weighted_score)
+    resource.refresh_from_db()
+    if resource.version_number == version:
+        resource.objects.update_score(earned, possible, modified)
 
 
 @CELERY_APP.task
 @set_code_owner_attribute
-def send_leaf_outcome(assignment_id, points_earned, points_possible):
+def send_leaf_score(resource_id, points_earned, points_possible, modified):
     """
     Calculate and transmit the score for a single problem. This method assumes
     that the individual problem was the source of a score update, and so it
     directly takes the points earned and possible values. As such it does not
     have to calculate the scores for the course, making this method far faster
-    than send_outcome_for_composite_assignment.
+    than sending an outcome for a composite module.
     """
-    assignment = GradedAssignment.objects.get(id=assignment_id)
-    if points_possible == 0:
-        weighted_score = 0
-    else:
-        weighted_score = float(points_earned) / float(points_possible)
-    outcomes.send_score_update(assignment, weighted_score)
+    assignment = LtiGradedResource.objects.get(id=resource_id)
+    assignment.objects.update_score(points_earned, points_possible, modified)
