60f9d217a36cfd025432e3e68a192331a27012f4
Geoff V <geoff@ibleducation.com>
2023-10-26 21:23:45 +0000

    feat: Add launch/<course_id>/<usage_id> endpoint
    - This is for displaying the content to the user

--- src/lti_1p3_provider/urls.py
+++ src/lti_1p3_provider/urls.py
@@ -8,6 +8,11 @@ from . import views
 
 urlpatterns = [
     path("login/", views.LtiToolLoginView.as_view(), name="lti-login"),
-    re_path("launch/", views.LtiToolLaunchView.as_view(), name="lti-launch"),
+    path("launch/", views.LtiToolLaunchView.as_view(), name="lti-launch"),
+    re_path(
+        f"launch/{settings.COURSE_ID_PATTERN}/{settings.USAGE_ID_PATTERN}$",
+        views.DisplayTargetResource.as_view(),
+        name="lti-display",
+    ),
     path("pub/jwks/", views.LtiToolJwksView.as_view(), name="lti-pub-jwks"),
 ]
