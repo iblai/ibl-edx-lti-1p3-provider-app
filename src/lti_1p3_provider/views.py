19bf5fcaf7247df8690fbd8bf35bde50271bfa09
Geoff V <geoff@ibleducation.com>
2023-11-15 19:20:45 +0000

    chore: Rm unused imports in views

--- src/lti_1p3_provider/views.py
+++ src/lti_1p3_provider/views.py
@@ -16,15 +16,13 @@ from urllib import parse
 
 from django.conf import settings
 from django.contrib.auth import authenticate, get_user_model, login
-from django.contrib.auth.decorators import login_required
 from django.http import (
     Http404,
     HttpResponse,
     HttpResponseBadRequest,
-    HttpResponseForbidden,
     JsonResponse,
 )
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect
 from django.urls import Resolver404, resolve, reverse
 from django.utils import timezone
 from django.utils.decorators import method_decorator
