09cc8c0fee5bbd5418d0b84a5851d9bbf84d4c3b
Geoff V <geoff@ibleducation.com>
2023-11-16 21:54:45 +0000

    test: fix test_reformatting_pub_key_error

--- src/lti_1p3_provider/tests/test_error_formatter.py
+++ src/lti_1p3_provider/tests/test_error_formatter.py
@@ -3,15 +3,17 @@ from lti_1p3_provider.error_formatter import reformat_error
 
 def test_reformatting_pub_key_error():
     """Reformats invalid response from jwks endpoint error"""
-    err_text = "Invalid response from https://some.website.com/thing/json-endpoint. "
-    "Must be JSON: <html><head></head><body>some text</body></html>."
+    err_text = (
+        "Invalid response from https://some.website.com/thing/json-endpoint. "
+        "Must be JSON: <html><head></head><body>some text</body></html>."
+    )
 
     expected = (
         "Error fetching platform public key. Invalid response from "
-        "https://some.website.com/thing/json-endpoint. Must be JSON."
+        "https://some.website.com/thing/json-endpoint. Must be JSON"
     )
 
-    assert reformat_error(err_text == expected)
+    assert reformat_error(err_text) == expected
 
 
 def test_no_formatting_applied_if_no_matches():
