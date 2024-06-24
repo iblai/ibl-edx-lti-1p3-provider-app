e352f6fc219aa96454b490514d027bd1b5e9b84b
Geoff Von Allmen <geoff@ibleducation.com>
2024-06-24 10:39:05 -0700

    refactor: Add LaunchGate to admin

--- src/lti_1p3_provider/admin.py
+++ src/lti_1p3_provider/admin.py
@@ -2,9 +2,16 @@ from django.contrib import admin
 
 from . import models
 
+
+@admin.register(models.LaunchGate)
+class LaunchGateAdmin(admin.ModelAdmin):
+    list_display = ("lti_tool__client_id",)
+
+
 @admin.register(models.LtiProfile)
 class LtiProfileAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(models.LtiGradedResource)
 class LtiPGradedResourceAdmin(admin.ModelAdmin):
