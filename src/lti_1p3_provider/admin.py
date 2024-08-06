7d07619ef743a24ab5224d255d9821dc5e85bbc5
Geoff V. <geoff@ibleducation.com>
2024-08-06 16:43:22 +0000

    fix: FK client_id list_display in admin

--- src/lti_1p3_provider/admin.py
+++ src/lti_1p3_provider/admin.py
@@ -5,7 +5,10 @@ from . import models
 
 @admin.register(models.LaunchGate)
 class LaunchGateAdmin(admin.ModelAdmin):
-    list_display = ("lti_tool__client_id",)
+    list_display = ("get_client_id",)
+
+    def get_client_id(self, obj):
+        return obj.client_id
 
 
 @admin.register(models.LtiProfile)
