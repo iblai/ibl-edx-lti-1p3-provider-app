b898be5858066cfad0b4b08c313af87b28af67aa
Geoff V. <geoff@ibleducation.com>
2024-08-06 19:52:59 +0000

    refactor: Add more fields to admin display

--- src/lti_1p3_provider/admin.py
+++ src/lti_1p3_provider/admin.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from django.contrib import admin
 
 from . import models
@@ -5,10 +7,19 @@ from . import models
 
 @admin.register(models.LaunchGate)
 class LaunchGateAdmin(admin.ModelAdmin):
-    list_display = ("get_client_id",)
+    list_display = ("tool_name", "has_allowed_keys", "allowed_orgs")
 
-    def get_client_id(self, obj):
-        return obj.client_id
+    def has_allowed_keys(self, obj) -> bool:
+        return bool(obj.allowed_keys)
+
+    def tool_name(self, obj):
+        return f"{obj.tool.title} ({obj.tool.client_id})"
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        formfield = super().formfield_for_foreignkey(db_field, request, **kwargs)
+        if db_field.name == "tool":
+            formfield.label_from_instance = lambda obj: f"{obj.title} ({obj.client_id})"
+        return formfield
 
 
 @admin.register(models.LtiProfile)
