054a04968df1d4531000b2c29ce9b0cb70842b93
Geoff V. <geoff@ibleducation.com>
2024-08-14 17:26:09 +0000

    refactor: Add issuer/client_id to admin

--- src/lti_1p3_provider/admin.py
+++ src/lti_1p3_provider/admin.py
@@ -7,15 +7,28 @@ from . import models
 
 @admin.register(models.LaunchGate)
 class LaunchGateAdmin(admin.ModelAdmin):
-    list_display = ("tool_name", "has_allowed_keys", "allowed_orgs")
+    list_display = (
+        "tool_name",
+        "tool_issuer",
+        "tool_client_id",
+        "has_allowed_keys",
+        "allowed_orgs",
+    )
 
     def has_allowed_keys(self, obj) -> bool:
         return bool(obj.allowed_keys)
 
-    def tool_name(self, obj):
-        return f"{obj.tool.title} ({obj.tool.client_id})"
+    def tool_name(self, obj) -> str:
+        return f"{obj.tool.title}"
+
+    def tool_issuer(self, obj) -> str:
+        return obj.tool.issuer
+
+    def tool_client_id(self, obj) -> str:
+        return obj.tool.client_id
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        """Shows human readable name for tool selection"""
         formfield = super().formfield_for_foreignkey(db_field, request, **kwargs)
         if db_field.name == "tool":
             formfield.label_from_instance = lambda obj: f"{obj.title} ({obj.client_id})"
@@ -34,15 +47,22 @@ class LtiGradedResourceAdmin(admin.ModelAdmin):
 
 @admin.register(models.LtiToolOrg)
 class LtiTooLOrgAdmin(admin.ModelAdmin):
-    list_display = ("tool_name", "org_name")
+    list_display = ("tool_name", "tool_issuer", "tool_client_id", "edx_org_name")
 
     def tool_name(self, obj) -> str:
-        return f"{obj.tool.title} ({obj.tool.client_id})"
+        return f"{obj.tool.title}"
 
-    def org_name(self, obj) -> str:
+    def tool_issuer(self, obj) -> str:
+        return obj.tool.issuer
+
+    def tool_client_id(self, obj) -> str:
+        return obj.tool.client_id
+
+    def edx_org_name(self, obj) -> str:
         return obj.org.short_name
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        """Shows human readable name for tool selection"""
         formfield = super().formfield_for_foreignkey(db_field, request, **kwargs)
         if db_field.name == "tool":
             formfield.label_from_instance = lambda obj: f"{obj.title} ({obj.client_id})"
