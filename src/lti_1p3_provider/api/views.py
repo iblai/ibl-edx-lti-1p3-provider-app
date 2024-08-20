cace5d50528eba2cc9cb203393e37468f7f3b69a
Geoff V. <geoff@ibleducation.com>
2024-08-20 17:17:28 +0000

    refactor: Add authentication/permission_classes

--- src/lti_1p3_provider/api/views.py
+++ src/lti_1p3_provider/api/views.py
@@ -1,8 +1,9 @@
 from django.utils.decorators import method_decorator
+from openedx.core.lib.api.authentication import BearerAuthenticationAllowInactiveUser
 from pylti1p3.contrib.django.lti1p3_tool_config.models import LtiToolKey
+from rest_framework.permissions import IsAdminUser
 from rest_framework.viewsets import ModelViewSet
 
-from ..models import LtiKeyOrg
 from ..views import requires_lti_enabled
 from .serializers import LtiToolKeySerializer
 
@@ -10,9 +11,8 @@ from .serializers import LtiToolKeySerializer
 @method_decorator(requires_lti_enabled, name="dispatch")
 class LtiKeyViewSet(ModelViewSet):
     serializer_class = LtiToolKeySerializer
-    # FIX: get correct authentication/permission classes
-    authentication_classes = []
-    permission_classes = []
+    authentication_classes = [BearerAuthenticationAllowInactiveUser]
+    permission_classes = [IsAdminUser]
 
     def get_queryset(self):
         return (
