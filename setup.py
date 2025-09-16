02108932d67b244327d2a69bff680dac2181e6d4
Geoff V <geoff@ibleducation.com>
2025-09-16 17:49:51 +0000

    build: version to 2.4.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="ibl-lti-1p3-provider",
-    version="2.3.7",
+    version="2.4.0",
     packages=find_packages("src"),
     include_package_data=True,
     package_dir={"": "src"},
