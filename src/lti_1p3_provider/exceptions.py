ef43234dae19cb1b09b412cf93342ef48790cb40
Geoff V <geoff@ibleducation.com>
2025-10-06 22:26:40 +0000

    refactor: Add DlBlockFilterError

--- src/lti_1p3_provider/exceptions.py
+++ src/lti_1p3_provider/exceptions.py
@@ -14,3 +14,18 @@ class DeepLinkingError(Lti1p3ProviderError):
         self.message = message
         self.status_code = status_code
         super().__init__(f"{title}: {message}")
+
+
+class DlBlockFilterError(Lti1p3ProviderError):
+    """Raised when there is an error with the deep linking block filter
+
+    Args:
+        developer_message: A message for the developer to help debug the issue
+        user_message: A message for the user to explain the issue
+
+    If no user_message is provided, a generic error will be returned.
+    """
+
+    def __init__(self, dev_message: str = "", user_message: str = "") -> None:
+        self.user_message = user_message
+        self.dev_message = dev_message or user_message
